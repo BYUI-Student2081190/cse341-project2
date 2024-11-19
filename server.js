8e3bf88c5572627015619350c8880be5195779bd
byui_student1045 <sco22022@byui.edu>
2024-11-19 16:10:46 -0700

    Added code to server.js and added a debugger.

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-// Hello!
+/** Required Varibles **/
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodbConnection = require('./database/connect');
+const port = process.env.PORT || 3030;
+const app = express();
+
+/** Middleware **/
+
+/** Routes **/
+
+/** Database - Running Port **/
